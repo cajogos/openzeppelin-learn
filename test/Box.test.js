b8e9ccb374eb79c4d045ddce60f6a0ba13c23eb9
Carlos Ferreira <carlosferreira@outlook.com>
2021-06-06 23:28:54 +0100

    OpenZeppelin Test Environment

--- test/Box.test.js
+++ test/Box.test.js
@@ -1,41 +1,38 @@
 // Load dependencies
+const { accounts, contract } = require('@openzeppelin/test-environment');
+const { expectEvent, expectRevert, BN } = require('@openzeppelin/test-helpers');
 const { expect } = require('chai');
 
-// Import utilities from Test Helpers (OpenZeppelin)
-const { BN, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
-
 // Load compiled artifacts
-const Box = artifacts.require('Box');
+const Box = contract.fromArtifact('Box');
 
 // Start test block
-contract('Box', function([ owner, other ]) {
+describe('Box', function() {
 
-    // Use large integers (bit numbers)
+    const [ owner, other ] = accounts;
+
     const value = new BN('42');
 
     beforeEach(async function() {
         // Deploy a new Box contract for each test
-        this.box = await Box.new({ from: owner });
+        this.contract = await Box.new({ from: owner });
     });
 
     it('retrieve returns a value previously stored', async function() {
-        await this.box.store(value, { from: owner });
+        await this.contract.store(value, { from: owner });
 
-        // Use large integer comparisons
-        expect(await this.box.retrieve()).to.be.bignumber.equal(value);
+        expect((await this.contract.retrieve()).toString()).to.equal('42');
     });
 
     it('store emits an event', async function() {
-        const receipt = await this.box.store(value, { from: owner });
-
-        // Test that a valueChanged event was emitted with the new value
+        const receipt = await this.contract.store(value, { from: owner });
         expectEvent(receipt, 'ValueChanged', { newValue: value });
     });
 
     it('non owner cannot store a value', async function() {
         // Test a transaction reverts
         await expectRevert(
-            this.box.store(value, { from: other }),
+            this.contract.store(value, { from: other }),
             'Ownable: caller is not the owner'
         );
     });
