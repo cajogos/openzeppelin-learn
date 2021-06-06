091186a43d4e716666e966f15f156843b12aca49
Carlos Ferreira <carlosferreira@outlook.com>
2021-06-06 21:59:06 +0100

    Adding more contracts

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -1,13 +1,24 @@
 pragma solidity ^0.6.0;
 
+// Import Auth from the access-control subdirectory
+import "./access-control/Auth.sol";
+
 contract Box {
     uint256 private value;
+    Auth private auth;
 
     // Emitted when the stored value changes
     event ValueChanged(uint256 newValue);
 
+    constructor(Auth _auth) public {
+        auth = _auth;
+    }
+
     // Stores a new value in the contract
     function store(uint256 newValue) public {
+        // Require that the caller is registered as an administrator in Auth
+        require(auth.isAdministrator(msg.sender), "Unauthorized");
+        
         value = newValue;
         emit ValueChanged(newValue);
     }
