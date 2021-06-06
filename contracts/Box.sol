c59d8ff40592f5eca41d09e21615721a42e65493
Carlos Ferreira <carlosferreira@outlook.com>
2021-06-06 22:09:08 +0100

    Upgrade solidity to 0.8.0

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -1,24 +1,21 @@
-pragma solidity ^0.6.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 // Import Auth from the access-control subdirectory
 import "./access-control/Auth.sol";
 
-contract Box {
+// Import Ownable from the OpenZeppelin Contracts library
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// Make Box inherit from the Ownable contract
+contract Box is Ownable {
     uint256 private value;
-    Auth private auth;
 
     // Emitted when the stored value changes
     event ValueChanged(uint256 newValue);
 
-    constructor(Auth _auth) public {
-        auth = _auth;
-    }
-
-    // Stores a new value in the contract
-    function store(uint256 newValue) public {
-        // Require that the caller is registered as an administrator in Auth
-        require(auth.isAdministrator(msg.sender), "Unauthorized");
-        
+    // The onlyOwner modifier restricts who can call the store function
+    function store(uint256 newValue) public onlyOwner {
         value = newValue;
         emit ValueChanged(newValue);
     }
